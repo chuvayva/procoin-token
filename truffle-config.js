94543b60adfeffab75fd05b7da6c20c8cc5d62ef
Vitaliy Kireev <chuvayva@gmail.com>
2018-03-07 12:34:56 +0400

    Ropsten deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,26 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv');
+
+const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
+const privKey = process.env["DEPLOYMENT_PRIVATE_KEY"];
+const infuraUrl = process.env["ETHEREUM_RPC_URL"];
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    ganache: {
+      host: 'localhost',
+      port: 7545,
+      network_id: '*',
+      gasPrice: 10000000000
+    },
+    ropsten: {
+      provider: () => {
+        return new HDWalletProvider(privKey, infuraUrl)
+      },
+      gas: 3000000,
+      gasPrice: 10000000000,
+      network_id: 3
+    }
+  }
 };
