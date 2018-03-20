86122c12757923eb05912279fdcc3fad34550083
Vitaliy Kireev <chuvayva@gmail.com>
2018-03-20 15:56:33 +0400

    Consisted totalSupply and balances on start

--- migrations/2_create_procoin_migration.js
+++ migrations/2_create_procoin_migration.js
@@ -3,7 +3,7 @@ let ProcoinDB = artifacts.require("./ProcoinDB.sol");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(ProcoinDB).then(function() {
-    return deployer.deploy(Procoin, 10e+6, 'Procoin', 'pro', ProcoinDB.address);
+    return deployer.deploy(Procoin, 0, 'Procoin', 'pro', ProcoinDB.address);
   });
 };
 
